bc71bafac764ef130d377c22315ae4be368dae94
Nikhil Arora <narora200@gmail.com>
2018-08-07 19:41:54 +0530

    [Feat]: Filtering done

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,18 @@
 const feed = (parent, args, context, info) => {
   const { db } = context;
-  return db.query.links({}, info);
+
+  const where = (
+    args.filter
+      ? {
+        OR: [
+          { url_contains: args.filter },
+          { description_contains: args.filter },
+        ],
+      }
+      : {}
+  );
+
+  return db.query.links({ where }, info);
 };
 
 export default { feed };
